3e421893036d69b438b4e5e1a884da82d982384e
G.Katerinchik <ggg140@yandex.ru>
2021-05-17 18:39:51 +0300

    добавлено 1 задание

--- src/task1.java
+++ src/task1.java
@@ -1,2 +1,31 @@
+import java.util.Arrays;
+
+public  class task1 {
+
+
+    public static void main(String[] args) {
+        replacer replacer = new replacer();
+        Integer[]arr=new Integer[]{1,2,3,4,5};
+        String[]arr2=new String[]{"a","b","C","d","e"};
+        replacer.replace(arr,1,2);
+        replacer.replace(arr2,1,2);
+
+
+    }
+}
+
+class replacer{
+    public  <T> void replace(T arr[], int i, int j) {
+        T a;
+        a = arr[i];
+        arr[i] = arr[j];
+        arr[j] = a;
+        for(int k=0; k< arr.length;k++){
+            System.out.println(arr[k]);
+        }
+
+    }
+
+}
 
 
